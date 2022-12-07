989929be3370a6eae0068df6deee410ea89a58d6
Solongos <kyj@gsa.hs.kr>
2022-12-07 21:23:40 +0900

    office

---
+++ python300/031-040.py
@@ -0,0 +1,25 @@
+# 분기 = "2020/03(E)  (iFRS연결)"
+
+# 분기_split = 분기.split('(')
+
+# print(분기_split[0])
+
+# data = "   삼성전자    "
+# # data_new = data.replace(' ', '')
+# data_new = data.strip()
+# print(data_new)
+
+# file_name = 'report.xlsx'
+# print(file_name.endswith('xlsx'))
+
+# movie_rank = list(('doctor strange', 'split', 'lucky'))
+# del movie_rank[1]
+# print(movie_rank)
+
+# lang1 = ['C', 'C++', 'JAVA']
+# lang2 = ['Python', 'Go', 'C#']
+# lang3 = lang1 + lang2
+# print(lang3)
+
+nums = [1, 2, 3, 4, 5, 6, 7]
+print(max(nums), min(nums), sum(nums), len(nums))
